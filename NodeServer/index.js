91833e16a8c76f4bc6a2846b4a82ad7f6ef786ce
soma_arc <somaarcr@gmail.com>
2017-05-31 22:20:37 +0900

    Send scene data using JSON-RPC

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -1,5 +1,27 @@
+const request = require('request');
 const chokidar = require('chokidar');
 const fs = require('fs');
+
+function MakeRequest (parameter) {
+    return {
+        "jsonrpc": "2.0",
+        "method": "render",
+        "params": parameter
+    }
+}
+
+function postRequest (data) {
+    const options = {
+        uri: "http://localhost:8081/rpc",
+        headers: {
+            "Content-type": "application/json",
+        },
+        json: data
+    };
+    request.post(options, function (error, response, body)  {
+        console.log(body);
+    });
+}
 
 let watchFilename = 'tmp/scene.json'
 if (process.argv[2] !== undefined) {
@@ -7,7 +29,10 @@ if (process.argv[2] !== undefined) {
 }
 
 chokidar.watch('/tmp/scene.json', {ignored: /[\/\\]\./}).on('all', (event, path) => {
-    const data = JSON.parse(fs.readFileSync('/tmp/scene.json', 'utf8'));
+    const data = JSON.parse(fs.readFileSync('/tmp/scene.json', 'utf8') || "null");
+    if (data === null) return;
     console.log(event, path);
-    console.log(JSON.stringify(data, null, 4));
+    const request = MakeRequest(data);
+    console.log(JSON.stringify(request, null, 4));
+    postRequest(request);
 });
